86db5f54046e835f2c56ace90286e4b8f7076b19
David Wogan <wogandavid@users.noreply.github.com>
2020-12-07 14:30:45 +0900

    update transform

--- workflow/scripts/transform.py
+++ workflow/scripts/transform.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from collections import OrderedDict
 
-
 # read raw data
 
 RawEGEDA = pd.read_excel('../../data/00APEC_EGEDA_20Feb2020.xlsx',sheet_name = None,na_values = ['x', 'X', '']) # I don't think there's any x's or X's in the EGEDA xlsx file, but leaving as is (shouldn't make a difference)
